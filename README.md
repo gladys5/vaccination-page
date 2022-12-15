b2d62af8fec2c7ae2f360a8dfaf0e562032e9be9
gladys5 <yumanji@live.com>
2022-12-14 20:05:28 -0700

    primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Proyecto Vacunacion
+
+![Capture vaccunacion](https://user-images.githubusercontent.com/85569433/207762474-f14b0316-e139-46db-8730-e24c17433e55.PNG)
---
+++ vaccination/script.js
@@ -0,0 +1,4 @@
+function Showmenu() {
+  document.getElementById("menu_items").classList.toggle("show")
+}
+const container = document.getElementById("div-sec3-right")
